199a50512adc55318c741f1a6ab8d6556056131c
Vitalii Horodetskyi <23rash23@gmail.com>
2024-01-27 18:34:37 +0200

    feat(hw-2): added homework-2

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
 const logger = require('./utils/logger')('main');
 
 logger.info('the script is running!');
+logger.warn('warning!');
+logger.error('error!');
--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,41 @@
+const colors = require('colors/safe');
+const config = require('config');
+
+// Reading environment variable values
+const COLORS_ENABLED = config.get('colorsEnabled');
+const LOG_LEVEL = config.get('logLevel');
+
+// Enabling colors if COLORS_ENABLED is set to 1
+if (Number(COLORS_ENABLED) === 1) {
+  colors.enable();
+} else {
+  colors.disable();
+}
+
 function logger(moduleName) {
+  // Define colors based on the log method
+  const colorMap = {
+    info: colors.blue,
+    warn: colors.yellow,
+    error: colors.red,
+  };
+
   return {
-    info: (...args) => console.log(`${moduleName}:`, ...args),
-    warn: (...args) => console.warn(`${moduleName}:`, ...args),
-    error: (...args) => console.error(`${moduleName}:`, ...args)
+    info: (...args) => {
+      if (LOG_LEVEL === 'info') {
+        console.log(colorMap.info(`${moduleName}:`), ...args);
+      }
+    },
+    warn: (...args) => {
+      if (LOG_LEVEL === 'info' || LOG_LEVEL === 'warn') {
+        console.warn(colorMap.warn(`${moduleName}:`), ...args);
+      }
+    },
+    error: (...args) => {
+      if (LOG_LEVEL === 'info' || LOG_LEVEL === 'warn' || LOG_LEVEL === 'error') {
+        console.error(colorMap.error(`${moduleName}:`), ...args);
+      }
+    },
   };
 }
 
