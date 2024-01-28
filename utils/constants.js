1241f44b93248f84c5d14315679aad2c5bc5a92f
Vitalii Horodetskyi <23rash23@gmail.com>
2024-01-28 12:12:12 +0200

    refactor(hw-2): refactored switching logger methods

---
+++ utils/constants.js
@@ -0,0 +1,20 @@
+const colors = require('colors/safe');
+
+// Define colors based on the log method
+const COLOR_MAP = {
+  info: colors.blue,
+  warn: colors.yellow,
+  error: colors.red,
+};
+
+// Define log level priority
+const LOG_LEVEL_PRIORITY = {
+  info: 1,
+  warn: 2,
+  error: 3,
+};
+
+module.exports = {
+  COLOR_MAP,
+  LOG_LEVEL_PRIORITY,
+};
--- utils/logger.js
+++ utils/logger.js
@@ -1,39 +1,31 @@
 const colors = require('colors/safe');
 const config = require('config');
+const { COLOR_MAP, LOG_LEVEL_PRIORITY } = require('./constants');
 
 // Reading environment variable values
 const COLORS_ENABLED = config.get('colorsEnabled');
 const LOG_LEVEL = config.get('logLevel');
 
 // Enabling colors if COLORS_ENABLED is set to 1
-if (Number(COLORS_ENABLED) === 1) {
-  colors.enable();
-} else {
+if (Number(COLORS_ENABLED) === 0) {
   colors.disable();
 }
 
 function logger(moduleName) {
-  // Define colors based on the log method
-  const colorMap = {
-    info: colors.blue,
-    warn: colors.yellow,
-    error: colors.red,
-  };
-
   return {
     info: (...args) => {
-      if (LOG_LEVEL === 'info') {
-        console.log(colorMap.info(`${moduleName}:`), ...args);
+      if (LOG_LEVEL_PRIORITY.info >= LOG_LEVEL_PRIORITY[LOG_LEVEL]) {
+        console.info(COLOR_MAP.info(`${moduleName}:`), ...args);
       }
     },
     warn: (...args) => {
-      if (LOG_LEVEL === 'info' || LOG_LEVEL === 'warn') {
-        console.warn(colorMap.warn(`${moduleName}:`), ...args);
+      if (LOG_LEVEL_PRIORITY.warn >= LOG_LEVEL_PRIORITY[LOG_LEVEL]) {
+        console.warn(COLOR_MAP.warn(`${moduleName}:`), ...args);
       }
     },
     error: (...args) => {
-      if (LOG_LEVEL === 'info' || LOG_LEVEL === 'warn' || LOG_LEVEL === 'error') {
-        console.error(colorMap.error(`${moduleName}:`), ...args);
+      if (LOG_LEVEL_PRIORITY.error >= LOG_LEVEL_PRIORITY[LOG_LEVEL]) {
+        console.error(COLOR_MAP.error(`${moduleName}:`), ...args);
       }
     },
   };
